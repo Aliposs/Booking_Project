e93e1913633f3be8dd247950ee6e6910247b2262
Your Name <you@example.com>
2024-05-08 14:12:39 +0300

    searchitem added successfully

---
+++ src/pages/components/searchitem/SearchItem.jsx
@@ -0,0 +1,231 @@
+import "./searchitem.css"
+
+const SearchItem = () => {
+
+    return (<div>
+
+    <div className="searchItem">
+        <img
+        src = "https://cf.bstatic.com/xdata/images/hotel/square240/433601691.webp?k=71cca59346ad4a5dcdb40073ca87526728230ccb5652d9159416f5d4afb9cb0f&o="
+        alt=""
+        className="siImg"
+        />
+        <div className="siDesc">
+            <h1 className="siTitle">El Gouna Sea & Garden Elite Villa's & Apartment's Private Residence - HurghadaOpens in new window</h1>
+            <span className="siDistance">22.3 km from centre</span>
+            <span className="siDealOp">Limited-time Deal</span>
+            <span className="siSubtitle">Family Studio</span>
+            <span className="siFeatures">Entire studio • 1 bathroom • 1 kitchen • 95m² Multiple bed types</span>
+            <span className="siServiceOp">Breakfast included</span>
+            <span className="siRemainingOp">Only 1 left at this price on our site</span>
+        </div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Fabulous</span>
+                <div>94 reviews</div>
+                <button>8.8</button>
+            </div>
+            <div className="siDetailTexts">
+                <span>1 night, 2 adults</span>
+            <span className="siPrice">EGP 4,747</span>
+            <span className="siTaxiOp">+EGP 2,241 taxes and charges</span>
+            <button className="siCheckButton">See Availability</button>
+            </div>
+        </div>
+
+    </div>
+
+
+    <div className="searchItem">
+        <img
+        src = "https://cf.bstatic.com/xdata/images/hotel/square240/537868801.webp?k=1064e7b7cfe6f06849fc8c094145caec4de41aaa20dd617dc414efdc90214d81&o="
+        alt=""
+        className="siImg"
+        />
+        <div className="siDesc">
+            <h1 className="siTitle">Davinci Beach Hotel</h1>
+            <span className="siDistance">1.1 km from centre</span>
+            <span className="siSubtitle">Double or Twin Room with City View</span>
+            <span>2 double beds</span>
+            <span className="siServiceOp">All-inclusive</span>
+            <div className="siColor">
+            <span>Free cancellation</span>
+            <div>No prepayment needed - pay at the property</div>
+            </div>
+            <span className="siRemainingOp">Only 6 left at this price on our site</span>
+        </div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Very good</span>
+                <div>177 reviews</div>
+                <button>8.1</button> 
+            </div>
+            <div className="siDetailTexts">
+            <span>1 night, 2 adults</span>
+            <span className="siPrice">EGP 1,571</span>
+            <span className="siTaxiOp">+EGP 424 taxes and charges</span>
+            <button className="siCheckButton">See Availability</button>
+            </div>
+        </div>
+
+    </div>
+
+    <div className="searchItem">
+        <img
+        src = "https://cf.bstatic.com/xdata/images/hotel/square240/409253399.webp?k=4c86f14122d0e2a71f135c94245d7eaef4cd06110dbee55cd3cd373c235e31a3&o="
+        alt=""
+        className="siImg"
+        />
+        <div className="siDesc">
+            <h1 className="siTitle">Lamera Hotel</h1>
+            <span className="siDistance">4.7 km from centre</span>
+            <span className="siDealOp">Gateway Deal</span>
+            <span className="siSubtitle">Large Double Room</span>
+            <span className="siFeatures">2 single beds</span>
+        </div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Very good</span>
+                <div>506 reviews</div>
+                <button>8.3</button>
+            </div>
+            <div className="siDetailTexts">
+                <span>1 night, 2 adults</span>
+            <span className="siPrice">EGP 898</span>
+            <span className="siTaxiOp">+EGP 150 taxes and charges</span>
+            <button className="siCheckButton">See Availability</button>
+            </div>
+        </div>
+
+    </div>
+
+    <div className="searchItem">
+        <img
+        src = "https://cf.bstatic.com/xdata/images/hotel/square240/445453761.webp?k=9fccc435122381dcb699da2dea18aee44d72072f83cc94142667d9d69a035027&o="
+        alt=""
+        className="siImg"
+        />
+        <div className="siDesc">
+            <h1 className="siTitle">Shellghada Blue Beach</h1>
+            <span className="siDistance"> 7.5 km from centre</span>
+            <span className="siSubtitle">Deluxe Double or Twin Room with Mountain View</span>
+            <span className="siFeatures">2 beds (1 single, 1 large double)</span>
+            <span className="siServiceOp">Breakfast included</span>
+            <div className="siColor">
+            <span>Free cancellation</span>
+            <div>No prepayment needed - pay at the property</div>
+            </div>
+            <span className="siRemainingOp">Only 2 rooms left at this price on our site</span>
+        </div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Very good</span>
+                <span>1.113 reviews</span>
+                <button>8.2</button>
+            </div>
+            <div className="siDetailTexts">
+                <span>1 night, 2 adults</span>
+            <span className="siPrice">EGP 1,980</span>
+            <span className="siTaxiOp">+EGP 297 taxes and charges</span>
+            <button className="siCheckButton">See Availability</button>
+            </div>
+        </div>
+
+    </div>
+
+    <div className="searchItem">
+        <img
+        src = "https://cf.bstatic.com/xdata/images/hotel/square240/421006026.webp?k=d97e1f9721ad9f020ff86ff1923a804e50f3f485bdd9d3cb09cfc0a82fdb8905&o="
+        alt=""
+        className="siImg"
+        />
+        <div className="siDesc">
+            <h1 className="siTitle">Zen Resort Sahl Hasheesh by TBH Hotels</h1>
+            <span className="siDistance">23.7 km from centre</span>
+            <span className="siSubtitle">Superior Double Room</span>
+            <span className="siFeatures">Beds: 1 double or 2 singles</span>
+            <span className="siServiceOp">Breakfast included</span>
+            <span className="siRemainingOp">Only 6 rooms left at this price on our site</span>
+        </div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Very good</span>
+                <div>1,144 reviews</div>
+                <button>8.5</button>
+            </div>
+            <div className="siDetailTexts">
+                <span>1 night, 2 adults</span>
+            <span className="siPrice">EGP 2,342</span>
+            <span className="siTaxiOp">+EGP 746 taxes and charges</span>
+            <button className="siCheckButton">See Availability</button>
+            </div>
+        </div>
+
+    </div>
+
+    <div className="searchItem">
+        <img
+        src="https://cf.bstatic.com/xdata/images/hotel/square240/304889800.webp?k=960208d1bd466aabff4ca1d44a489cdc6f64305a5aec9cc5b03d48b6a43f81bc&o="
+        alt=""
+        className="siImg"
+        />
+        <div className="siDesc">
+            <h1 className="siTitle">Masaya Hurghada Rooms</h1>
+            <span className="siDistance">3.6 km from centre</span>
+            <span className="siSubtitle">Deluxe Double Room</span>
+            <span className="siFeatures">1 large double bed</span>
+            <span className="siRemainingOp">Only 2 rooms left at this price on our site</span>
+        </div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Fabulous</span>
+                <div>332 reviews</div>
+                <button>8.9</button>
+            </div>
+            <div className="siDetailTexts">
+                <span>1 night, 2 adults</span>
+            <span className="siPrice">EGP 1.078</span>
+            <span className="siTaxiOp">+EGP 291 taxes and charges</span>
+            <button className="siCheckButton">See Availability</button>
+            </div>
+        </div>
+
+    </div>
+
+    <div className="searchItem">
+        <img
+        src="https://cf.bstatic.com/xdata/images/hotel/square240/498977898.webp?k=917f68f117f8fbc4858e5bfa2febd48cdcd81acb99f8cff8fdd04c72dda09e8b&o="
+        alt=""
+        className="siImg"
+        />
+        <div className="siDesc">
+            <h1 className="siTitle">Palm Inn Suites Hotel</h1>
+            <span className="siDistance">100 m from centre</span>
+            <span className="siSubtitle">Budget Double Room</span>
+            <span className="siFeatures">Beds: 1 double or 2 singles</span>
+            <span className="siServiceOp">Breakfast included</span>
+            <span className="siRemainingOp">Only 4 rooms left at this price on our site</span>
+        </div>
+        <div className="siDetails">
+            <div className="siRating">
+                <span>Good</span>
+                <div>463 reviews</div>
+                <button>7.8</button>
+            </div>
+            <div className="siDetailTexts">
+                <span>1 night, 2 adults</span>
+            <span className="siPrice">EGP 984</span>
+            <span className="siTaxiOp">+EGP 266 taxes and charges</span>
+            <button className="siCheckButton">See Availability</button>
+            </div>
+        </div>
+
+    </div>
+
+
+    
+
+</div>
+);};
+
+export default SearchItem;
