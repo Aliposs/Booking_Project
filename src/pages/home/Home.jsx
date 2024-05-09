3942159eb09a58b76a7f979313259e3e940e6947
Your Name <you@example.com>
2024-05-09 10:05:01 +0300

    propertyListFiles added successfully

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,15 @@
 import "./home.css";
 import Featured from "../components/propertyList/Featured";
 import FeaturedProperties from "../components/featuredProperties/FeaturedProperties";
+import PropertyList from "../components/propertyList/PropertyList";
+
 
 const Home = () => {
     return(
         <div>
             <div className="homeContainer">
             <h1 className="homeTitle"> Browes by property type</h1>
+            <PropertyList/>
             <h1 className="homeTitle"> Trending destination</h1>
             <Featured/>
             <h1 className="homeTitle"> Homes Guests Love</h1>
